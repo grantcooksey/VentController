078ce7ca4a332a9e19e07d31024f88bb04f5d7b4
cooksey <suitcase24@gmail.com>
2023-02-07 12:00:53 -0500

    Code skeleton

--- firmware/sketch/sketch.ino
+++ firmware/sketch/sketch.ino
@@ -1,14 +1,40 @@
-#define LED_PIN 0
+#define PIN_SDA PB0
+#define PIN_SCL PB2
+#define PIN_ANALOG_HUMIDITY_SHUTOFF_LEVEL PB3
+#define PIN_MOTOR_RELAY PB4
+
+// 0-1023 (10 bit ADC) on ATTinys
+#define ADC_MAX_RANGE 1023
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_PIN, OUTPUT);
+    pinMode(PIN_MOTOR_RELAY, OUTPUT);
+    pinMode(PIN_ANALOG_HUMIDITY_SHUTOFF_LEVEL, INPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(250);                       // wait for a second
-  digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(250);                       // wait for a second
+    // Read trimmer value
+    int humidityShutoffLevel = readHumidityShutoffLevel();
+    int currentHumidityLevel = readCurrentHumidityLevel();
+    if (currentHumidityLevel > humidityShutoffLevel) {
+        enableFan();
+    } else {
+        disableFan();
+    }
+}
+
+void enableFan() {
+    return;
+}
+
+void disableFan() {
+    return;
+}
+
+int readCurrentHumidityLevel() {
+    return 1;
+}
+
+int readHumidityShutoffLevel() {
+    return 1;
 }
